094b3be2c88c1861cec9b2b2bd7551cea83b215b
Francis Chua <francischua689@gmail.com>
2025-04-29 10:30:18 -0700

    add test that adding lines works

--- src/tests/main.ts
+++ src/tests/main.ts
@@ -1,4 +1,5 @@
 import puppeteer from "puppeteer-core";
+import path from "path";
 
 (async () => {
   const browserExecutablePath = process.argv.at(2);
@@ -9,7 +10,7 @@ import puppeteer from "puppeteer-core";
   }
   const browser = await puppeteer.launch({
     executablePath: browserExecutablePath,
-    headless: false,
+    headless: true,
   });
   const page = await browser.newPage();
   page.on("console", (msg) =>
@@ -27,7 +28,68 @@ import puppeteer from "puppeteer-core";
     console.log(request.failure()?.errorText, request.url);
   });
   await page.goto("http://localhost:8080");
-  await new Promise((r) => setTimeout(() => r(0), 3000));
-  // other actions...
+  await test_enter(page);
   await browser.close();
 })();
+
+// We should be able to enter two lines.
+async function test_enter(page: puppeteer.Page) {
+  await page.type(".line", "i32.const 1");
+  await page.keyboard.press("Enter");
+  await page.type(".line", "i32.const 2");
+  assert((await page.$$(".line")).length == 2, "should now have two lines");
+}
+
+const colors = {
+  reset: "\x1b[0m",
+  bright: "\x1b[1m",
+  dim: "\x1b[2m",
+  underscore: "\x1b[4m",
+  blink: "\x1b[5m",
+  reverse: "\x1b[7m",
+  hidden: "\x1b[8m",
+
+  fg: {
+    black: "\x1b[30m",
+    red: "\x1b[31m",
+    green: "\x1b[32m",
+    yellow: "\x1b[33m",
+    blue: "\x1b[34m",
+    magenta: "\x1b[35m",
+    cyan: "\x1b[36m",
+    white: "\x1b[37m",
+    gray: "\x1b[90m",
+    crimson: "\x1b[38m", // Scarlet
+  },
+  bg: {
+    black: "\x1b[40m",
+    red: "\x1b[41m",
+    green: "\x1b[42m",
+    yellow: "\x1b[43m",
+    blue: "\x1b[44m",
+    magenta: "\x1b[45m",
+    cyan: "\x1b[46m",
+    white: "\x1b[47m",
+    gray: "\x1b[100m",
+    crimson: "\x1b[48m",
+  },
+};
+
+function assert(value: unknown, msg: string) {
+  if (!value) {
+    const error = new Error();
+    const directory = path.resolve(path.dirname(""));
+    const location = error
+      .stack!.toString()
+      .split(/\r\n|\n/)
+      .slice(2)
+      .map(
+        (location) =>
+          "  " + location.trim().replace(`file://${directory}/`, ""),
+      )
+      .join("\n");
+    console.log(
+      `${colors.fg.red}Failed assertion: ${msg} ${colors.reset} ${colors.bright}${colors.dim}\n${location}${colors.reset}`,
+    );
+  }
+}
